6d1ce6637dd0b32c8331f6a706e755fc0fb52373
chituenguyen <tuenguyen553@gmail.com>
2022-03-04 14:06:07 +0700

    UI for login/register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,84 +1,121 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
-// import logo from '../assets/img/logo.png';
-import '../styles/Register.css';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import React, { useState, useEffect } from "react";
+import {
+  Grid,
+  Card,
+  Typography,
+  List,
+  ListItem,
+  Button,
+} from "@material-ui/core";
 
-// import { registerUser } from '../redux/auth/authSlice';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    backgroundColor: "#e6e0e0",
+    height: "100vh",
+  },
+  text20: {
+    fontFamily: "Poppins",
+    fontWeight: "normal",
+    fontSize: "20px",
+    lineHeight: "30px",
+    letterSpacing: "0.01rem",
+    color: "#192A3E",
+    fontWeight: 600,
+    marginBottom: "16px",
+  },
+  text13: {
+    fontFamily: "Poppins",
+    fontWeight: "normal",
+    fontSize: "13px",
+    lineHeight: "19px",
+    letterSpacing: "0.01rem",
+    color: "#FFF",
+    fontWeight: 600,
+  },
+  input: {
+    width: "400px",
+    height: "48px",
+    borderRadius: "8px",
+    marginBottom: "12px",
+    outline: "none",
+    padding: "16px 12px",
+    border:"1px solid #5D5FEF",
+    boxShadow:"4px 4px 1px rgba(36,37,94,0.1) ",
+  },
+  label: {
+    marginBottom: "16px",
+    fontFamily: "DM Sans",
+    fontWeight: 500,
+    fontSize: "18px",
+    lineHeight: "24px",
+    color: "#404D61",
+  },
+  button: {
+    width: "155px",
+    height: "40px",
+  },
+}));
 
-const Register = () => {
-  // const dispatch = useDispatch();
-  // const { isAuthenticated } = useSelector((state) => state.auth);
+export default function Signin() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  // const [formData, setFormData] = useState({
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   password: '',
-  // });
-
-  // const { firstName, lastName, email, password } = formData;
-
-  // const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  // const handleSubmit = () => {
-  //   dispatch(registerUser({ firstName, lastName, email, password }));
-  // };
-
-  // if (isAuthenticated) {
-  //   return <Redirect to="/" />;
-  // }
   return (
-    <>
-      {/* <div id="register_bg"></div>
-      <div id="register">
-        <figure>
-          <Link to="/">
-            <img src={logo} width="50" height="50" alt="" />
-          </Link>
-        </figure>
-        <div class="divider">
-          <span></span>
-        </div>
-        <form autocomplete="off">
-          <div class="form-group">
-            <input class="form-control" type="text" name="firstName" placeholder="First Name" onChange={onChange} />
-            <i class="icon_pencil-edit"></i>
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="text" name="lastName" placeholder="Last Name" onChange={onChange} />
-            <i class="icon_pencil-edit"></i>
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="email" name="email" placeholder="Email" onChange={onChange} />
-            <i class="icon_mail_alt"></i>
-          </div>
-          <div class="form-group">
-            <input
-              class="form-control"
-              type="password"
-              name="password"
-              id="password1"
-              placeholder="Password"
-              onChange={onChange}
-            />
-            <i class="icon_lock_alt"></i>
-          </div>
-          <button type="button" class="btn_register" onClick={handleSubmit}>
-            Register Now!
-          </button>
-          <div class="mt-2">
-            <small>
-              Already have an acccount?{' '}
-              <strong>
-                <Link to="/login">Sign In</Link>
-              </strong>
-            </small>
-          </div>
-        </form>
-      </div> */}
-    </>
-  );
-};
+    <Grid container direction="row" spacing={0} className={classes.root}>
+      <Grid item xs={6}>
+        <img
+          src="https://st.quantrimang.com/photos/image/2019/04/24/multiplebackground-1.jpg"
+          style={{ height: "100%", width: "100%" }}
+        ></img>
+      </Grid>
+      <Grid
+        item
+        xs={6}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card style={{ padding: "35px 47px", height: "547px", width: "488px" }}>
+          <Typography className={classes.text20}>
+            Let's become one of us{" "}
+          </Typography>
 
-export default Register;
+          <form>
+            <label className={classes.label}>Username</label>
+            <br></br>
+            <input type="text" className={classes.input}></input>
+            <br></br>
+            <label className={classes.label}>Password</label>
+            <br></br>
+            <input type="password" className={classes.input}></input>
+            <br></br>
+            <label className={classes.label}>Confirm Password</label>
+            <br></br>
+            <input type="password" className={classes.input}></input>
+            <br></br>
+          </form>
+          <List>
+            <ListItem>
+              <Button variant="contained" color="primary">
+                Sign up
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ marginLeft: "auto" }}
+              >
+                Sign In
+              </Button>
+            </ListItem>
+          </List>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
