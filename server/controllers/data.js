7ec52ed9fcf0727b0c81f258f134b8df4d411fe8
HoaiNam071001 <nam215543694@gmail.com>
2022-03-26 07:43:28 +0700

    nam

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -7,14 +7,21 @@ exports.getData = async (req,res) =>{
         var data = []; 
         var start = new Date(req.body.start);
         var end = new Date(req.body.end);
+        var min = 1024,max = -1,avg = 0,n = 0;
         if(start.toString() === 'Invalid Date' || end.toString() === "Invalid Date") 
             throw {message:'Invalid Date'};
         datadb.forEach((res)=>{
             if(res.time.getTime() >= start.getTime() &&
             res.time.getTime() <= end.getTime())
+            {
+                n++;
                 data.push({'val':res.value,'time':res.time});
+                if(Number(res.value) < min) min = Number(res.value);
+                if(Number(res.value) > max) max = Number(res.value);
+                avg += Number(res.value);
+            }
         });
-        res.status(200).send(data);
+        res.status(200).send({"min":min,"max":max,"avg":avg/n,"data":data});
     } catch (err) {
         if(err.message ==='Invalid Date')
             res.status(400).send(err);
